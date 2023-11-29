15c89130c08006fdd3a62deb1e160ca2d913643e
MD AL AMIN KHAN <122734003+alaminkhan786@users.noreply.github.com>
2023-11-29 13:02:20 +0600

    second commit

--- Task - 4/script.js
+++ Task - 4/script.js
@@ -1,17 +1,17 @@
 let mark = Number(prompt('Enter your mark: '))
 
 if (mark >= 80 && mark <= 100 ){
-    document.write('A+')
+    document.write('Your grade is: A+')
 } else if (mark >= 70 && mark < 80) {
-    document.write('A')
+    document.write('Your grade is: A')
 }else if (mark >= 60 && mark < 70) {
-    document.write('B')
+    document.write('Your grade is: B')
 }else if (mark >= 50 && mark < 60) {
-    document.write('C')
+    document.write('Your grade is: C')
 }else if (mark >= 40 && mark < 50) {
-    document.write('D')
+    document.write('Your grade is: D')
 }else if (mark < 40 && mark >= 0) {
-    document.write('F')
+    document.write('Your grade is: F')
 }else {
-    document.write('Wrong Entry')
+    document.write('Invalid Prompt')
 }
